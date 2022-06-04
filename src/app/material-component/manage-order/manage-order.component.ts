c4b0d8532aadd6837f5438415b0643f95f5b2f5f
prabath1998 <prabhathudayanga1998@gmail.com>
2022-06-04 22:23:04 +0530

    changes in bills

--- src/app/material-component/manage-order/manage-order.component.ts
+++ src/app/material-component/manage-order/manage-order.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { BillService } from 'src/app/services/bill.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstants } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-manage-order',
@@ -32,6 +33,77 @@ export class ManageOrderComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.ngxService.start();
+    this.getCategories();
+    this.manageOrderForm = this.formBuilder.group({
+      name:[null,[Validators.required,Validators.pattern(GlobalConstants.nameRegex)]],
+      email:[null,[Validators.required,Validators.pattern(GlobalConstants.emailRegex)]],
+      contactNumber:[null,[Validators.required,Validators.pattern(GlobalConstants.contactNumberRegex)]],
+      paymentMethod:[null,[Validators.required]],
+      product:[null,[Validators.required]],
+      category:[null,[Validators.required]],
+      quantity:[null,[Validators.required]],
+      price:[null,[Validators.required]],
+      total:[0,[Validators.required]],
+    })
+  }
+
+  getCategories(){
+    this.categoryService.getCategories().subscribe((response:any)=>{
+      this.ngxService.stop();
+      this.categories = response;
+      // console.log(this.categories);
+      
+    },(error:any)=>{
+      this.ngxService.stop();
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
+  }
+
+  getProductsByCategory(value:any){
+    this.productService.getProductsByCategory(value.id).subscribe((response:any)=>{
+      this.products = response;
+      this.manageOrderForm.controls['price'].setValue('');
+      this.manageOrderForm.controls['quantity'].setValue('');
+      this.manageOrderForm.controls['total'].setValue(0);
+    },(error:any)=>{
+      this.ngxService.stop();
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
+  }
+
+  getProductDetails(value:any){
+    this.productService.getById(value.id).subscribe((response:any)=>{
+      this.price = response.price;
+      this.manageOrderForm.controls['price'].setValue(response.price);
+      this.manageOrderForm.controls['quantity'].setValue('1');
+      this.manageOrderForm.controls['total'].setValue(this.price*1);
+    },(error:any)=>{
+      this.ngxService.stop();
+      if (error.error?.message) {
+        this.responseMessage = error.error?.message;
+      }else{
+        this.responseMessage = GlobalConstants.genericError;
+      }
+      this.snackbarService.openSnackBar(this.responseMessage,GlobalConstants.error);
+    })
+  }
+
+  setQuantity(value:any){
+    var temp = this.manageOrderForm.controls['quantity'].value;
+    if (temp > 0) {
+      this.manageOrderForm.controls['total'].setValue()
+    }
   }
 
 }
